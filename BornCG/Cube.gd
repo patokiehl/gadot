2fa1ad1b6dfa27dc892eee8400f65ae895293d20
ARLJohnston <40921666+ARLJohnston@users.noreply.github.com>
2023-02-25 08:49:40 +0000

    no gaps

---
+++ BornCG/Cube.gd
@@ -0,0 +1,21 @@
+extends StaticBody
+
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	var myMesh = MeshInstance.new()
+	myMesh.mesh = CubeMesh.new()
+	myMesh.mesh.set_size(Vector3(50,100,50))
+	myMesh.show()
+	add_child(myMesh)
+	#pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
